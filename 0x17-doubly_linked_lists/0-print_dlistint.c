84e054fa2e517a2e1e0b505d0cbdf6c0e50657e2
TinyBila <bilatinym@gmail.com>
2023-09-16 22:23:11 -0700

    updated function that prints all the elements of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,7 +7,7 @@
  * @h: head of the list
  * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	int count;
 
@@ -27,4 +27,4 @@ size_t print_dlistint(const dlistint_t *h);
 	}
 
 	return (count);
-}	
+}
